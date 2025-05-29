7baf783f610dfb2351a2316e3d33dfe4ab9ffa3c
Muhammed Shabnas P A <muhammedshabnaspa@gmail.com>
2025-05-29 12:06:05 +0530

    Add files via upload

---
+++ SQL/Banking_Analysis_Sql.sql
@@ -0,0 +1,13 @@
+create database Banking_Analysis;
+
+use Banking_Analysis;
+
+show tables;
+
+select * from customer;
+
+SHOW COLUMNS FROM customer;
+
+ALTER TABLE customer
+CHANGE `ï»¿Client ID` `Client ID` CHAR(8);
+
